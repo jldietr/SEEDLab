6e4f9c1020d8295fa23972ea7e5b01de65ed1e3e
reluben <76892349+reluben@users.noreply.github.com>
2021-10-05 14:23:05 -0600

    Update motortest.ino

--- motortest.ino
+++ motortest.ino
@@ -1,18 +1,8 @@
-#define TIMER_INTERRUPT_DEBUG         0
-#define _TIMERINTERRUPT_LOGLEVEL_     0
-
-#define USE_TIMER_2 true
-
-
 #include<Encoder.h>
 #include<math.h>
-#include<TimerInterrupt.h>
+
 
 int motorPin = 9;
-unsigned long time_now = 0;
-int period = 50;
-int PWMvalue = 200;
-
 
 Encoder myEnc(3, 5);
 
@@ -21,41 +11,12 @@ long int oldPosition  = -999;
 double Kp = 0.21;
 int given = 0;
 double Ki = 0.172;
-double Kd = 0;
-bool lastFlag;
 int intThreshholdCounts = 250;
 unsigned long currentTime = 0;
 double integral = 0;
 int analog;
 int closeEnough = 10;
 long int newPosition;
-
-void controller() {
-  newPosition = myEnc.read();
-  if (newPosition - given > 0) {
-    digitalWrite(7, HIGH);
-  }
-  if (newPosition - given < 0) {
-    digitalWrite(7, LOW);
-  }
-  analog = Kp * (double)abs(newPosition - given) + integral * Ki;
-  if (analog > 255) {
-    analog = 255;
-  }
-  analogWrite(motorPin, analog);
-  if (abs(newPosition - given) < intThreshholdCounts) {
-    integral += (double)abs(newPosition - given) * 0.05;
-  }
-  else if (abs(newPosition - given) > intThreshholdCounts) {
-    integral = 0;
-  }
-  if (abs(newPosition - given) < closeEnough) {
-    integral = 0;
-  }
-}
-
-#define TIMER_INTERVAL_MS 50
-
 
 void setup() {
   // put your setup code here, to run once:
@@ -74,15 +35,7 @@ void setup() {
   pinMode(11, INPUT);
   pinMode(13, INPUT);
 
-  ITimer2.init();
-  // interval (in ms) and duration (in milliseconds). Duration = 0 or not specified => run indefinitely
 
-
-   // Interval in unsigned long millisecs
-  if (ITimer.attachInterruptInterval(TIMER_INTERVAL_MS, controller))
-    Serial.println("Starting  ITimer OK, millis() = " + String(millis()));
-  else
-    Serial.println("Can't set ITimer. Select another freq. or timer");
 
 }
 
@@ -110,30 +63,43 @@ void loop() {
   else if (digitalRead(11) == HIGH && digitalRead(13) == HIGH) {
     given = 2400;
   }
- /* while (newPosition - given < 0 || newPosition - given > 0) {
 
+
+
+  if (newPosition - given < 0 || newPosition - given > 0) {
     currentTime = millis();
     newPosition = myEnc.read();
     analog = Kp * (double)abs(newPosition - given) + integral * Ki;
     if (analog > 255) {
       analog = 255;
     }
+    if (analog < 0) {
+      analog = 0;
+    }
     analogWrite(motorPin, analog);
-
     if (newPosition - given > 0) {
       digitalWrite(7, HIGH);
     }
     if (newPosition - given < 0) {
       digitalWrite(7, LOW);
     }
-
     if (abs(newPosition - given) < intThreshholdCounts) {
       integral += (double)abs(newPosition - given) * 0.05;
     }
-
     Serial.println((double)newPosition * (PI / 1600.0));
-    while (millis() < currentTime + 50);
   }
-  integral = 0;*/
 
+
+
+  
+  if (abs(newPosition - given) < intThreshholdCounts) {
+    integral += (double)abs(newPosition - given) * 0.05;
+  }
+  else if (abs(newPosition - given) > intThreshholdCounts) {
+    integral = 0;
+  }
+  if (abs(newPosition - given) < closeEnough) {
+    integral = 0;
+  }
+  while (millis() < currentTime + 50);
 }
